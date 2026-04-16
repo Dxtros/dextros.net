fae9bb76c9e7855fc11a4bf01bccb33b6d3058d1
Dexter Delaski <git@dextros.net>
2026-04-16 10:22:41 -0700

    site update

--- app/__generated__/$resources.assets.ts
+++ app/__generated__/$resources.assets.ts
@@ -1,3 +1,14 @@
 
-    export const assets = {};
+    export const assets = {
+  "2ac2613d81d68605da0a403850f27b6178d2e496fe7241ae918aeac675364187": {
+    "url": "/cgi/image/i_cK8cZ4fxHhPYdEEZy07bv.webp?format=raw",
+    "width": 480,
+    "height": 308
+  },
+  "9478aa2f02c907a2549eced9c9fe13f9dc3858d568330793e10bd05d448c16c7": {
+    "url": "/cgi/image/funny-dumbo-rat-peeks-out-behind-hand-veterinarian-blue-uniform-rat-has-cute-funny-expression-259550503.jpg_D0H62OO6dTCZ1fu_uEYr_.webp?format=raw",
+    "width": 1600,
+    "height": 934
+  }
+};
     
