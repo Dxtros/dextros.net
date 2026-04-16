8378a71fc4e22be1526aaf3132b1f0407ab4fb69
Dexter Delaski <git@dextros.net>
2026-04-15 20:39:30 -0700

    initial commit

---
+++ app/__generated__/$.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable */
+      /* This is a auto generated file for building the project */ 
+
+
+      import { Fragment, useState } from "react";
+      import { useResource, useVariableState } from "@webstudio-is/react-sdk/runtime";
+      import { Body as Body, Link as Link } from "@webstudio-is/sdk-components-react-router";
+import { HtmlEmbed as HtmlEmbed } from "@webstudio-is/sdk-components-react";
+
+
+      export const projectId = "a31fd28d-415c-4f03-8fc1-93b07e834ea3";
+
+      export const lastPublished = "2026-04-16T01:28:08.208Z";
+
+      export const siteName = "dextros.net";
+
+      export const breakpoints = [{"id":"p8eOIktc7fhlZ0xcFxuYL"},{"id":"gLCzUI5P79jD1WN4LFS5F","maxWidth":991},{"id":"btFHh22tjla-HCaKSSn8k","maxWidth":767},{"id":"Vmpp1WIlOdATwrho_J4oG","maxWidth":479}];
+
+      export const favIconAsset: string | undefined =
+        undefined;
+
+      // Font assets on current page (can be preloaded)
+      export const pageFontAssets: string[] =
+        []
+
+      export const pageBackgroundImageAssets: string[] =
+        []
+
+      
+
+      const Page = (_props: { system: any; }) => {
+return <Body
+className={`w-element c1pmamtn c1xgflv3 c1jo5tbg c5tf688`}>
+<div
+className={`w-element`}>
+<div
+className={`w-element c2z4q9g cuyj2c cze7tgv c1uf6gp5 c16shzhh c1jwwzyo`}>
+<div
+className={`w-element`}>
+{"404"}
+</div>
+<div
+className={`w-element c1rvee0s c1gnstvv c12hnoj c1s84xtw c1c0ddk cl4ktzx`}>
+{"404"}
+</div>
+<div
+className={`w-element c1rvee0s c1gnstvv cf8jutm c1s84xtw cxhlit2 cl4ktzx`}>
+{"404"}
+</div>
+<div
+className={`w-element c1rvee0s cwimzng cpwzg5c cs9gzdy c5tf688 c16u47l2`} />
+</div>
+<p
+className={`w-element c2ec6lv covlakf c1gu6afd cdv4tzb cdzpd3k`}>
+{"PAGE NOT FOUND"}
+</p>
+</div>
+<Link
+href={"https://webstudio.is/?via=badge"}
+target={"_blank"}
+className={`w-element c1vndec1 cz6uwb c9g51w8 c1jo5tbg c1xgflv3 c1qina7l c1uir0hv c10qemqc cwtem0s c1a4nwk4 c9902ha cpmnivi c131pgw4 ctc53nv cnsq5pm c1jg1df0 cl95v1y c16shzhh cgel4jb csvk2wu c1mzwfmi cwegbxl c85x781 c1ttd7a8 coy7tla c1hqpf3q c1ax4f02 c1atyyva`}>
+<HtmlEmbed
+code={"<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"currentColor\" viewBox=\"0 0 16 16\" width=\"100%\" height=\"100%\" style=\"display: block;\"><path fill-rule=\"evenodd\" d=\"m12.32 12.416 2.62-8.085a1.205 1.205 0 1 0-2.292-.746l-2.62 8.084a1.205 1.205 0 1 0 2.292.747Z\" clip-rule=\"evenodd\"/><path fill-rule=\"evenodd\" d=\"M8 7.624c.297 0 .517.175.704.394.207.243.373.545.514.866.634 1.44.753 3.241.753 3.241a1.206 1.206 0 0 0 1.285 1.122 1.207 1.207 0 0 0 1.12-1.287s-.16-2.25-.952-4.05C10.744 6.364 9.594 5.208 8 5.208c-1.594 0-2.744 1.156-3.424 2.7-.792 1.8-.951 4.05-.951 4.05a1.207 1.207 0 0 0 1.12 1.288 1.206 1.206 0 0 0 1.284-1.122s.119-1.8.753-3.24a3.52 3.52 0 0 1 .514-.867c.187-.22.406-.394.704-.394Z\" clip-rule=\"evenodd\"/><path fill-rule=\"evenodd\" d=\"M5.973 11.669 3.352 3.585a1.205 1.205 0 1 0-2.293.746l2.622 8.084a1.205 1.205 0 1 0 2.292-.746Z\" clip-rule=\"evenodd\"/></svg>"}
+className={`w-html-embed c9fk8uj c1xgicr8 c9k7mbd ca9uefi`} />
+<div
+className={`w-element`}>
+{"Built with Webstudio"}
+</div>
+</Link>
+</Body>
+}
+
+
+      export { Page }
+    
---
+++ app/__generated__/_index.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable */
+      /* This is a auto generated file for building the project */ 
+
+
+      import { Fragment, useState } from "react";
+      import { useResource, useVariableState } from "@webstudio-is/react-sdk/runtime";
+      import { Body as Body, Link as Link } from "@webstudio-is/sdk-components-react-router";
+import { NavigationMenu as NavigationMenu, NavigationMenuList as NavigationMenuList, NavigationMenuItem as NavigationMenuItem, NavigationMenuTrigger as NavigationMenuTrigger, NavigationMenuContent as NavigationMenuContent, NavigationMenuLink as NavigationMenuLink } from "@webstudio-is/sdk-components-react-radix";
+import { Button as Button, Text as Text, Box as Box, HtmlEmbed as HtmlEmbed, Paragraph as Paragraph } from "@webstudio-is/sdk-components-react";
+
+
+      export const projectId = "a31fd28d-415c-4f03-8fc1-93b07e834ea3";
+
+      export const lastPublished = "2026-04-16T01:28:08.208Z";
+
+      export const siteName = "dextros.net";
+
+      export const breakpoints = [{"id":"p8eOIktc7fhlZ0xcFxuYL"},{"id":"gLCzUI5P79jD1WN4LFS5F","maxWidth":991},{"id":"btFHh22tjla-HCaKSSn8k","maxWidth":767},{"id":"Vmpp1WIlOdATwrho_J4oG","maxWidth":479}];
+
+      export const favIconAsset: string | undefined =
+        undefined;
+
+      // Font assets on current page (can be preloaded)
+      export const pageFontAssets: string[] =
+        []
+
+      export const pageBackgroundImageAssets: string[] =
+        []
+
+      
+            
+
+            export const CustomCode = () => {
+              return (<></>);
+            }
+          
+
+      const Page = (_props: { system: any; }) => {
+return <Body
+className={`w-element`}>
+<div
+className={`w-element c1pmamtn colidgn cdepr1w`}>
+<h1
+className={`w-element`}>
+{"Testing WEBSITE FLOW"}
+</h1>
+</div>
+</Body>
+}
+
+
+      export { Page }
+    
